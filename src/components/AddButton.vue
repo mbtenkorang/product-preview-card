c7ec15836f0eb5a0640d4342d1ec82579f5b4a25
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-28 01:03:46 +0000

    Initial commit

---
+++ src/components/AddButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <button>
+            <span></span>
+            {{ button_name }}
+        </button>
+    </div>
+</template>
---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const _productinfo = reactive({
+    category: "Perfume",
+    name: "Gabrielle Essence Eau De Parfum",
+    images: {
+        mobile: "./images/image-product-mobile.jpg",
+        desktop: "./images/image-product-desktop.jpg"
+    },
+    description: "A floral, solar and voluptuous interpretation composed by Olivier Polge, \
+  Perfumer-Creator for the House of CHANEL.",
+    current_price: 149.99,
+    previous_price: 169.99
+})
+
+const BUTTON_LABEL = ref("Add to Cart")
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
