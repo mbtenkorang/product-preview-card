34927a553f0346efbe7ed9e90c28a9e6fb8e910e
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-28 22:14:53 +0000

    Adjust styles for mobile

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -7,11 +7,11 @@ const props = defineProps({
 
 <template>
     <div class="text-sm font-normal border border-dark-cyan bg-dark-cyan rounded-lg text-neutral-white flex justify-center">
-        <button :name="button_name" aria-pressed="false" class="flex items-center py-4">
-            <span>
+        <button :name="button_name" aria-pressed="false" class="flex items-center py-5">
+            <span class="mr-2">
                 <img :src="icon_url" alt="" aria-hidden="true">
             </span>
-            <span>{{ button_name }}</span>
+            <span class="font-bold">{{ button_name }}</span>
         </button>
     </div>
 </template>
--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -13,18 +13,18 @@ const BUTTON_LABEL = reactive({
 </script>
 
 <template>
-    <picture class="w-full">
+    <picture class="">
         <source :srcset="product_data.images.desktop" media="(min-width:790px)" />
-        <img :src="product_data.images.mobile" alt="Picture of Chanel Perfume">
+        <img :src="product_data.images.mobile" alt="Picture of Chanel Perfume" class="w-full">
     </picture>
 
-    <div class="py-5 px-6">
+    <div class="py-4 px-6">
         <span class="text-xs tracking-[8px]">{{ product_data.category.toUpperCase() }}</span>
-        <h1 class="font-serif font-bold text-3xl text-dark-blue my-4">{{ product_data.name }}</h1>
-        <p class="text-sm text-gray-blue font-normal mb-4">{{ product_data.description }}</p>
-        <div class="font-serif flex mb-4">
-            <p class="text-dark-cyan text-3xl font-bold">${{ product_data.current_price }}</p>
-            <span class="text-[10px] text-gray-cyan"><s>${{ product_data.previous_price }}</s></span>
+        <h1 class="font-serif font-bold text-4xl text-dark-blue my-5">{{ product_data.name }}</h1>
+        <p class="text-base text-gray-blue font-normal">{{ product_data.description }}</p>
+        <div class="font-serif flex my-7">
+            <p class="text-dark-cyan text-4xl mr-6 font-bold">${{ product_data.current_price }}</p>
+            <span class="text-base text-gray-blue"><s>${{ product_data.previous_price }}</s></span>
         </div>
         <AddButton :icon_url="BUTTON_LABEL.icon" :button_name="BUTTON_LABEL.name" />
     </div>
