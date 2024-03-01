8ce0e44c2ffddba18508907716429faca3a332c5
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-01 21:38:56 +0000

    Remove unused style tag

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@ const _productinfo = reactive({
   images: {
     mobile: "./images/image-product-mobile.jpg",
     desktop: "./images/image-product-desktop.jpg",
-
   },
   description: "A floral, solar and voluptuous interpretation composed by Olivier Polge, \
   Perfumer-Creator for the House of CHANEL.",
@@ -23,4 +22,3 @@ const _productinfo = reactive({
   <ProductComponent :product_data="_productinfo" />
 </template>
 
-<style scoped></style>
